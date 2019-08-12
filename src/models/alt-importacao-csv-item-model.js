78d58e355db4ceb49756e492501ecedf9775f196
Vitor Hugo Nogueira de Souza <nogueira.dsn.shop@alterdata.matriz>
2019-08-12 11:09:10 -0300

    Commit teste nova branche.

--- src/models/alt-importacao-csv-item-model.js
+++ src/models/alt-importacao-csv-item-model.js
@@ -4,9 +4,10 @@
   ng.module('alt.importacao-csv')
     .factory('AltImportacaoCsvItemModel', ['AltImportacaoCsvResumoItemModel', function(ResumoItemImportacao) {
       class ItemImportacao {
-        constructor(linha, objeto) {
+        constructor(linha, objeto, teste) {
           this.linha = linha;
-          this.objeto = objeto;
+          this.objeto = objeto; // Obs: teste
+          this.teste = teste;
         }
       }
 
